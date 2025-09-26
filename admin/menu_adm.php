c3d66126b1d9cd3f0556c67902e3bcdb36ef0f04
felipe.fabris.ferreira.26@gmail.com <felipe.fabris.ferreira.26@gmail.com>
2025-09-26 17:09:27 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -2,49 +2,75 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Área Administrativa</title>
-    <!-- Link arquivos Bootstrap -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <!-- Link para CSS específico -->
-    <link rel="stylesheet" href="../css/meu_estilo.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Área Administrativa</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <!-- CSS personalizado -->
+  <link rel="stylesheet" href="../css/meu_estilo.css">
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
 <body class="fundofixo">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img src="../imagens/logochurrascopequeno.png" alt="" width="120"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">ADMIN</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">PRODUTOS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">TIPOS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">USUÁRIOS</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-danger me-2" type="submit"><i class="bi bi-house-fill"></i></button>
-        <button class="btn btn-outline-success" type="submit"><i class="bi bi-door-open-fill"></i></button>
-      </form>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <!-- Logo -->
+      <a class="navbar-brand" href="#"><img src="../imagens/logochurrascopequeno.png" alt="" width="120"></a>
+
+      <!-- Botão mobile -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Itens Navbar -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <!-- ms-auto joga tudo p/ direita -->
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex">
+
+          <!-- Botão usuário -->
+          <li class="nav-item me-2 order-1 d-flex">
+            <button type="button" class="btn btn-danger disabled me-2">
+              Olá, <?php echo($_SESSION['login_usuario']); ?>
+            </button>
+
+            <!-- Botões Home e Logout: colados ao lado do usuário no mobile -->
+            <div class="d-flex d-lg-none">
+              <a href="index.php" class="btn btn-secondary me-2"><i class="bi bi-house-fill"></i></a>
+              <a href="logout.php" class="btn btn-secondary"><i class="bi bi-door-open-fill"></i></a>
+            </div>
+          </li>
+
+          <!-- Itens do menu -->
+          <li class="nav-item order-2">
+            <a class="nav-link" href="../admin/index.php">ADMIN</a>
+          </li>
+          <li class="nav-item order-2">
+            <a class="nav-link" href="../admin/produtos_lista.php">PRODUTOS</a>
+          </li>
+          <li class="nav-item order-2">
+            <a class="nav-link" href="../admin/">TIPOS</a>
+          </li>
+          <li class="nav-item order-2">
+            <a class="nav-link" href="#">USUÁRIOS</a>
+          </li>
+        </ul>
+
+        <!-- Botões Home e Logout (só aparecem no desktop, depois do menu) -->
+        <div class="d-none d-lg-flex ms-2">
+          <a href="index.php" class="btn btn-secondary me-2"><i class="bi bi-house-fill"></i></a>
+          <a href="logout.php" class="btn btn-secondary"><i class="bi bi-door-open-fill"></i></a>
+        </div>
+
+      </div>
     </div>
-  </div>
-</nav>   
-<!-- Link arquivos Bootstrap js         
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>-->
+  </nav>
+
+  <!-- Bootstrap JS -->
+  <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
